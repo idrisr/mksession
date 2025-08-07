7c60020bcf7697220aca31e981805eda0c0fb8e8
Idris Raja <idris.raja@gmail.com>
2025-08-06 19:44:35 -0500

    update executable name and abstract over all systems

--- flake.nix
+++ flake.nix
@@ -5,33 +5,39 @@
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        compiler = "ghc984";
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          compiler = "ghc984";
+          mksession = pkgs.haskell.packages.${compiler}.callPackage ./default.nix { };
+        in
+        {
+          packages.default = mksession;
+          checks.default = mksession;
 
-        mksession = pkgs.haskell.packages.${compiler}.callPackage ./default.nix { };
-      in
-      {
-        packages.default = mksession;
-        checks.default = mksession;
-        overlays = final: prev: { mksession = final.haskell.packages.${compiler}.callPackage ./default.nix { }; };
-
-        devShells.default = pkgs.mkShell {
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.zlib ];
-          buildInputs = with pkgs.haskell.packages.${compiler}; [
-            ghc
-            cabal-install
-            ghcid
-            fourmolu
-            cabal-fmt
-            implicit-hie
-            cabal2nix
-            pkgs.ghciwatch
-            pkgs.haskell-language-server
-            pkgs.zlib
-            pkgs.colordiff
-          ];
-        };
-      });
+          devShells.default = pkgs.mkShell {
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.zlib ];
+            buildInputs = with pkgs.haskell.packages.${compiler}; [
+              ghc
+              cabal-install
+              ghcid
+              fourmolu
+              cabal-fmt
+              implicit-hie
+              cabal2nix
+              pkgs.ghciwatch
+              pkgs.haskell-language-server
+              pkgs.zlib
+              pkgs.colordiff
+            ];
+          };
+        }) //
+    {
+      overlays.default = final: prev: {
+        mksession =
+          final.haskell.packages.ghc984.callPackage ./default.nix { };
+      };
+    }
+  ;
 }
